fc07b26d1a8c4e1a239c2c289c37823261aa3c7d
raxmatillo <milodcomposer@gmail.com>
2023-04-09 13:45:53 +0500

    Config class

--- blog/schemas.py
+++ blog/schemas.py
@@ -7,3 +7,11 @@ from pydantic import BaseModel
 class Blog(BaseModel):
     title: str
     body: str
+
+
+class ShowBlog(BaseModel):
+    # agar
+    title: str
+    body: str
+    class Config():
+        orm_mode = True
